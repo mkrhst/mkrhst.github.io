6c2c58410dfcc1be4d7f80ca7bb6222fb860a6ac
mkrhst <makarenko_ihor@mail.ru>
2020-07-07 10:59:41 +0300

    v1
    v1

---
+++ js/index.js
@@ -0,0 +1,55 @@
+$(document).ready(function () {
+
+   $( "#open" ).click(function() {
+  
+	$( "#form" ).show();
+	
+	
+	});
+  
+  
+  
+  
+  
+  
+  
+  
+	$( "#close" ).click(function() {
+	$( "#form" ).hide();
+	
+});
+	
+ $( ".open1" ).click(function() {
+  
+	$( "#form" ).show();
+	
+	
+	});
+  
+  
+  
+  
+  
+  
+  
+  
+	$( "#close" ).click(function() {
+	$( "#form" ).hide();
+	
+});
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+});
+
